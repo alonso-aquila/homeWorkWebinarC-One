c97b0466c25f8cf516cb0d0138e23eb81db1dae0
Alonso <alonso.aquila@outlook.it>
2023-08-04 00:44:17 -0500

    Добавил код решения задач

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿//  ЗАДАЧА 1 
+
+  Console.WriteLine("Пожалуйста введите первое число:"); 
+ int numOne = int.Parse(Console.ReadLine());
+
+  Console.WriteLine("Пожалуйста введите второе число:"); 
+ int numTwo = int.Parse(Console.ReadLine());
+
+ if (numOne > numTwo)
+ {
+     Console.WriteLine("Первое больше второго числа");
+    // return;
+ }
+ if (numOne < numTwo)
+ {
+     Console.WriteLine("Второе число больше первого числа");
+     return;
+ }
+ else
+ {
+     Console.WriteLine("Числа равны");
+     return;
+ }
+
+
+
+//  ЗАДАЧА 2
+
+Console.WriteLine("Пожалуйста введите первое число:"); 
+int numOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста введите второе число:"); 
+int numTwo = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста введите третье число:"); 
+int numThree = int.Parse(Console.ReadLine());
+
+int max = numOne;
+
+if (max < numTwo)
+{
+  max = numTwo;
+}
+if (max < numThree)
+{
+  max = numThree;
+}
+Console.WriteLine("Максимальное число ");
+Console.WriteLine(max);
+
+
+
+// ЗАДАЧА 3
+
+Console.WriteLine("Пожалуйста введите число:"); 
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Это число четное");
+}
+else 
+{
+  Console.WriteLine("Это число нечетное");
+}
+
+
+
+// ЗАДАЧА 4
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int numberEven = 2;
+
+if(number > 1)
+{
+    while(numberEven <= number)
+    {
+        Console.WriteLine(numberEven);
+        numberEven = numberEven + 2;
+    }
+}
