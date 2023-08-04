110a5d08c6a9921c8736afbcd700dfd1e0a14080
Alonso <alonso.aquila@outlook.it>
2023-08-04 12:07:52 -0500

    Коммит из расширения

--- Program.cs
+++ Program.cs
@@ -1,83 +1,85 @@
-﻿//  ЗАДАЧА 1 
+﻿// //  ЗАДАЧА 1 
 
-  Console.WriteLine("Пожалуйста введите первое число:"); 
- int numOne = int.Parse(Console.ReadLine());
+//   Console.WriteLine("Пожалуйста введите первое число:"); 
+//  int numOne = int.Parse(Console.ReadLine());
 
-  Console.WriteLine("Пожалуйста введите второе число:"); 
- int numTwo = int.Parse(Console.ReadLine());
+//   Console.WriteLine("Пожалуйста введите второе число:"); 
+//  int numTwo = int.Parse(Console.ReadLine());
 
- if (numOne > numTwo)
- {
-     Console.WriteLine("Первое больше второго числа");
-    // return;
- }
- if (numOne < numTwo)
- {
-     Console.WriteLine("Второе число больше первого числа");
-     return;
- }
- else
- {
-     Console.WriteLine("Числа равны");
-     return;
- }
-
-
-
-//  ЗАДАЧА 2
-
-Console.WriteLine("Пожалуйста введите первое число:"); 
-int numOne = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Пожалуйста введите второе число:"); 
-int numTwo = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Пожалуйста введите третье число:"); 
-int numThree = int.Parse(Console.ReadLine());
-
-int max = numOne;
-
-if (max < numTwo)
-{
-  max = numTwo;
-}
-if (max < numThree)
-{
-  max = numThree;
-}
-Console.WriteLine("Максимальное число ");
-Console.WriteLine(max);
+//  if (numOne > numTwo)
+//  {
+//      Console.WriteLine("Первое больше второго числа");
+//     // return;
+//  }
+//  if (numOne < numTwo)
+//  {
+//      Console.WriteLine("Второе число больше первого числа");
+//      return;
+//  }
+//  else
+//  {
+//      Console.WriteLine("Числа равны");
+//      return;
+//  }
 
 
 
-// ЗАДАЧА 3
+// //  ЗАДАЧА 2
 
-Console.WriteLine("Пожалуйста введите число:"); 
-int number = int.Parse(Console.ReadLine());
+// Console.WriteLine("Пожалуйста введите первое число:"); 
+// int numOne = int.Parse(Console.ReadLine());
 
-if (number % 2 == 0)
-{
-    Console.WriteLine("Это число четное");
-}
-else 
-{
-  Console.WriteLine("Это число нечетное");
-}
+// Console.WriteLine("Пожалуйста введите второе число:"); 
+// int numTwo = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Пожалуйста введите третье число:"); 
+// int numThree = int.Parse(Console.ReadLine());
+
+// int max = numOne;
+
+// if (max < numTwo)
+// {
+//   max = numTwo;
+// }
+// if (max < numThree)
+// {
+//   max = numThree;
+// }
+// Console.WriteLine("Максимальное число ");
+// Console.WriteLine(max);
 
 
 
-// ЗАДАЧА 4
+// // ЗАДАЧА 3
 
-Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+// Console.WriteLine("Пожалуйста введите число:"); 
+// int number = int.Parse(Console.ReadLine());
 
-int numberEven = 2;
+// if (number % 2 == 0)
+// {
+//     Console.WriteLine("Это число четное");
+// }
+// else 
+// {
+//   Console.WriteLine("Это число нечетное");
+// }
 
-if(number > 1)
-{
-    while(numberEven <= number)
-    {
-        Console.WriteLine(numberEven);
-        numberEven = numberEven + 2;
-    }
-}
+
+
+// // ЗАДАЧА 4
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// int numberEven = 2;
+
+// if(number > 1)
+// {
+//     while(numberEven <= number)
+//     {
+//         Console.WriteLine(numberEven);
+//         numberEven = numberEven + 2;
+//     }
+// }
+
+
